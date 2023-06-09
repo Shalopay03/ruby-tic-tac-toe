808d9f715fb3bd01128dfb8da147f76bb3d08cde
Shalopay03 <134221829+Shalopay03@users.noreply.github.com>
2023-06-09 13:53:04 +0300

    add play_round method

--- main.rb
+++ main.rb
@@ -9,38 +9,47 @@ end
 
 class Game
   def initialize
-    @field = [
-      %w[1 2 3],
-      %w[4 5 6],
-      %w[9 8 7]
-    ]
+    @field = %w[1 2 3 4 5 6 7 8 9]
   end
 
-  def play
+  # playing game method
+  def play(player_one, _player_two)
+    play_round(player_one)
     show_field
   end
 
+  # outputs game field
   def show_field
-    puts @field[0][0] + ' | ' + @field[0][1] + ' | ' + @field[0][2]
+    puts @field[0] + ' | ' + @field[1] + ' | ' + @field[2]
     puts '---------'
-    puts @field[1][0] + ' | ' + @field[1][1] + ' | ' + @field[1][2]
+    puts @field[3] + ' | ' + @field[4] + ' | ' + @field[5]
     puts '---------'
-    puts @field[2][0] + ' | ' + @field[2][1] + ' | ' + @field[2][2]
+    puts @field[6] + ' | ' + @field[7] + ' | ' + @field[8]
+  end
+
+  # gets player choice and places x or o on the field
+  def play_round(player)
+    choice = nil
+    loop do
+      puts player.name + ', choose the avaible cell between 1 and 9:'
+      choice = gets.chr
+      break if @field.include? choice and choice != 'x' and choice != 'o'
+    end
+    @field[@field.find_index(choice)] = player.symbol
   end
 end
 
-def initialize_player
+def initialize_player(symbol)
   puts 'Enter player name: '
   name = gets.chop
-  puts 'Enter player symbol: '
-  symbol = gets.chop
   Player.new(name, symbol)
 end
 
 puts 'First player initialization:'
-player_one = initialize_player
+player_one = initialize_player('x')
 puts 'Second player initialization:'
-player_one = initialize_player
+player_two = initialize_player('o')
 
 game = Game.new
-game.play
+game.show_field
+game.play(player_one, player_two)
