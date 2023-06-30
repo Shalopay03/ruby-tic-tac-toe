25516d87b13a5faa519abc8b3dad4e7dfbe3afc5
Shalopay03 <134221829+Shalopay03@users.noreply.github.com>
2023-06-30 13:05:04 +0300

    add #play method test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,6 +5,7 @@ require_relative '../lib/game'
 describe Game do
   subject(:game_one) { described_class.new }
   let(:player_one) { double(Player, name: 'player_one', symbol: 'x') }
+  let(:player_two) { double(Player, name: 'player_two', symbol: 'o') }
   describe '#play_round' do
     before do
       allow($stdout).to receive(:puts)
@@ -67,6 +68,39 @@ describe Game do
       it 'returns true' do
         game_one.instance_variable_set(:@field, %w[x 2 3 4 x 6 7 8 x])
         expect(game_one.won?(player_one)).to eq(true)
+      end
+    end
+  end
+
+  describe '#play' do
+    context 'plays rounds' do
+      before do
+        allow(game_one).to receive(:won?).and_return(false)
+        allow(game_one).to receive(:show_field)
+        allow(game_one).to receive(:play_round)
+      end
+      it 'checks whether players won' do
+        expect(game_one).to receive(:won?).at_least(2).times
+        game_one.play(player_one, player_two)
+      end
+      it 'outputs field after each round' do
+        expect(game_one).to receive(:show_field).at_least(2).times
+        game_one.play(player_one, player_two)
+      end
+      it 'plays rounds' do
+        expect(game_one).to receive(:play_round).at_least(2).times
+        game_one.play(player_one, player_two)
+      end
+    end
+    context 'If player 2 wins on fourth turn' do
+      before do
+        allow(game_one).to receive(:won?).with(player_two).and_return(false, false, false, true)
+        allow(game_one).to receive(:won?).with(player_one).and_return(false, false, false, false)
+        allow(game_one).to receive(:show_field)
+        allow(game_one).to receive(:play_round)
+      end
+      it 'loops 4 times' do
+        expect(game_one.play(player_one, player_two)).to eq(player_two.name)
       end
     end
   end
